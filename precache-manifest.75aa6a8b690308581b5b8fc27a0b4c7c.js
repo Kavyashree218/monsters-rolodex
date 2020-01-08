13e84a36d6b7829c320381ec666c00d1f0da60a6
Kavya-venkat <kavyavenkat218@gmail.com>
2020-01-08 17:52:28 +0530

    Updates

---
+++ precache-manifest.75aa6a8b690308581b5b8fc27a0b4c7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "f97041017c4d139f60e3",
+    "url": "/monsters-rolodex/static/js/main.35139870.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f97041017c4d139f60e3",
+    "url": "/monsters-rolodex/static/css/main.b4c14aa3.chunk.css"
+  },
+  {
+    "revision": "3bbb3b5044dbbb2767c1272edccedbf7",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
